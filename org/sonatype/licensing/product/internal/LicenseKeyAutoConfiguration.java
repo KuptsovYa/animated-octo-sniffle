42a907af8f445714e650facfd16f539ac7ad172f
KuptsovYa <kuptsovyaroslav@yandex.ru>
2025-08-03 20:14:26 +0400

    4

---
+++ org/sonatype/licensing/product/internal/LicenseKeyAutoConfiguration.java
@@ -0,0 +1,26 @@
+package org.sonatype.licensing.product.internal;
+
+import java.util.Map;
+
+import org.sonatype.licensing.LicenseKey;
+import org.sonatype.licensing.feature.Feature;
+import org.sonatype.licensing.feature.Features;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class LicenseKeyAutoConfiguration {
+
+    @Bean
+    public Features features(Map<String, Feature> featureMap) {
+        return new Features(featureMap);
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public LicenseKey licenseKey(Features features) {
+        return new DefaultLicenseKey(features);
+    }
+}
